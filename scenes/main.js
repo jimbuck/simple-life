cad3b28cc10abb4d8d501b9e3fd4525747085beb
Jim Buck <jim@jimmyboh.com>
2013-02-12 12:34:55 -0500

    Changed from raw entities to Organisms.

--- scenes/main.js
+++ scenes/main.js
@@ -1,12 +1,24 @@
-﻿Crafty.scene('main', function(){
-	console.log('Main...');
-	var ground = Crafty.e('2D, Canvas, Color, Collision, Floor')
-        .color('black')
-        .attr({w:960,h:60,x:0,y:420});
+﻿//var colors = ['red','blue','yellow','orange','purple','cyan', 'magenta', 'black'];
 
-    var player = Crafty.e('2D, Canvas, Color, Twoway, Gravity')
-        .color('red')
-        .attr({w:50, h:50, x:100, y:100})
-        .twoway(5, 5)
-        .gravity('Floor').gravityConst(.4);
+
+var lowerColor = 3;
+var upperColor = 6;
+var allColors = [];
+
+Crafty.scene('main', function(){
+	Crafty.background('#608341');
+	
+	for(var r=lowerColor;r<=upperColor;r++){
+		for(var g=lowerColor;g<=upperColor;g++){
+			for(var b=lowerColor;b<=upperColor;b++){
+				allColors.push('#'+r+g+b);
+			}
+		}
+	}
+	
+	console.log(allColors.length + ' Organisms...');
+	
+	for(var c in allColors){
+		Crafty.e('Organism').organism(allColors[c]);
+	}
 });
