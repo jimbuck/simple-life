744099b2d323faff18a30c102093efed9c9777c1
Jim Buck <jim@jimmyboh.com>
2013-02-12 12:33:52 -0500

    Implemented the `renderMethod` variable.

--- scenes/loading.js
+++ scenes/loading.js
@@ -1,7 +1,7 @@
 ﻿var content = [];
 
 Crafty.scene("loading", function () {
-	console.log('Loading...');
+
 	// Preload content...
   if(content.length > 0){
 	  Crafty.load(content, function () {
@@ -10,7 +10,7 @@ Crafty.scene("loading", function () {
 	  
 	  // Black background with 'Loading' text.
 	  Crafty.background("#000");
-	  Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 })
+	  Crafty.e("2D, "+renderMethod+", Text").attr({ w: 100, h: 20, x: 150, y: 120 })
 	    .text("Loading")
 		  .css({ "text-align": "center" });
 
