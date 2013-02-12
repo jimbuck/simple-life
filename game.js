7d67596b2b0cfa36740799dce957c6b783c6655a
Jim Buck <jim@jimmyboh.com>
2013-02-12 12:33:32 -0500

    Added control buttons and styling.

--- game.js
+++ game.js
@@ -1,9 +1,26 @@
-﻿var tileSize = 16;
+﻿var renderMethod = 'Canvas';
+var tileSize = 16;
+var gameWidth = 64*tileSize;
+var gameHeight = 32*tileSize;
 
 $(function()
 {
-	Crafty.init(960, 480);
-	Crafty.canvas.init();
-	
+	$('ARTICLE').width(gameWidth+4);
+
+	Crafty.init(gameWidth, gameHeight);
+	if(renderMethod==='Canvas')
+		Crafty.canvas.init();
+
 	Crafty.scene("loading");
+	
+	$('#startStopButton').click(function(){
+		Crafty.pause();
+		$(this).val(Crafty.isPaused() ? 'Start' : 'Stop' );
+	});
+	
+	$('#resetButton').click(function(){
+			Crafty.pause(false);
+			$('#startStopButton').val('Stop');
+			Crafty.scene("main");
+	});
 });
