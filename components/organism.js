bcb4eac2789b1028ba1afe096113788ed971f3e6
Jim Buck <jim@jimmyboh.com>
2013-02-12 12:34:14 -0500

    Added reusable component to create organisms.

---
+++ components/organism.js
@@ -0,0 +1,11 @@
+﻿Crafty.c("Organism", {
+	init: function(){
+		this.requires('2D, '+renderMethod+', Color, XDNA')
+		.attr({w:tileSize, h:tileSize, x:tileSize*32, y:tileSize*16})
+		.color('#abc')
+		.xdna();
+	},
+	organism: function(color){
+		this.color(color);
+	}
+});
